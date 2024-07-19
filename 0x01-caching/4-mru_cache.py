fee5125df2891053b76b2529247da6beeb9dca0b
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-19 12:30:31 +0300

    final implementation

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,36 +1,50 @@
-#!/usr/bin/python3
-""" MRU Caching """
+#!/usr/bin/env python3
+# -*- Coding: utf-8 -*-
+"""
+Created on 19 July 2024
 
+@Author James Gatheru
+"""
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
     """
-    MRU Caching system that inherits from BasicCache
+    MRUCache class
     """
-
     def __init__(self):
+        """Constructor"""
         super().__init__()
         self.queue = []
 
     def put(self, key, item):
-        """Adding item to the cache"""
+        """
+        Add an item to the cache
+
+        Args:
+            key (str): key
+            item (str): item
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.queue:
                 self.queue.append(key)
             else:
                 self.queue.append(
-                    self.queue.pop(self.queue.index(key)))
-
+                        self.queue.pop(self.queue.index(key)))
             if len(self.queue) > BaseCaching.MAX_ITEMS:
-                removed_key = self.queue.pop(-2)
-                del self.cache_data[removed_key]
-                print("DISCARD: {}".format(removed_key))
+                removed = self.queue.pop(-2)
+                del self.cache_data[removed]
+                print("DISCARD: {}".format(removed))
 
     def get(self, key):
-        """Get an item by key"""
+        """
+        Retrieves an item from the cache
+
+        Args:
+            key (str): key
+        """
         if key is None or key not in self.cache_data:
             return None
         self.queue.append(self.queue.pop(self.queue.index(key)))
-        return self.cache_data(key)
+        return self.cache_data[key]
