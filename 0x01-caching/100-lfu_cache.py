0c62f89017e21f5fe99f87e8d223577692600d4f
JamesWainaina <jamesgatheru9905@gmail.com>
2024-07-19 14:24:27 +0300

    LFU Caching

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+"""
+LFU caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """
+    LFUCache class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        self.queue = []
+        self.counter = {}
+
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        item_count = self.counter.get(key, None)
+
+        if item_count is not None:
+            self.counter[key] += 1
+        else:
+            self.counter[key] = 1
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first = self.get_first_list(self.queue)
+            if first:
+                self.queue.pop(0)
+                del self.cache_data[first]
+                del self.counter[first]
+                print("DISCARD: {}".format(first))
+
+        if key not in self.queue:
+            self.queue.insert(0, key)
+        self.move_right_list(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        item = self.cache_data.get(key, None)
+        if item is not None:
+            self.counter[key] += 1
+            self.move_right_list(key)
+        return item
+
+    def move_right_list(self, item):
+        """
+        Move element to the right, taking into account LFU
+        """
+        length = len(self.queue)
+
+        idx = self.queue.index(item)
+        item_count = self.counter[item]
+
+        for i in range(idx, length):
+            if i != (length - 1):
+                nxt = self.queue[i + 1]
+                nxt_count = self.counter[nxt]
+
+                if nxt_count > item_count:
+                    break
+
+        self.queue.insert(i + 1, item)
+        self.queue.remove(item)
+
+    @staticmethod
+    def get_first_list(array):
+        """
+        Get the first element of the list
+        """
+        return array[0] if array else None
